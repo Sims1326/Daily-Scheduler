a51a6ad9cfb03aa3678bbee8537566e22e45c546
Anthony Sims <sims.anthonylevell@gmail.com>
2022-03-14 23:30:55 -0500

    created javascript function to compare current time to task time

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,26 @@
 var today = moment().format("dddd, MMMM Do YYYY");
 $('#currentDay').append(today)
 
+var currentHour = moment().hour();
+ console.log(currentHour)
+var taskHour = $(".time-block");
+
+taskHour.each(function(){
+    var taskTime = parseInt($(this).attr('id'))
+     console.log(taskTime)
+    if(taskTime === currentHour){
+        $(this).addClass("present")
+    }
+    else if(taskTime < currentHour){
+        $(this).addClass("past")
+    }
+    else {
+        $(this).addClass("future")
+    };
+
+
+ });
+
+
+// How do you get hours and minutes without date?
+// Compare current hour to taskTime
